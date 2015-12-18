beaf9bd50ba66702817fe7f648508d209f58bf65
Christian Fobel <christian@fobel.net>
2015-12-17 19:54:15 -0500

    Refactor common base message in schema

--- zmq_plugin/schema.py
+++ zmq_plugin/schema.py
@@ -6,14 +6,16 @@
 
 MESSAGE_SCHEMA = {
     'definitions':
-    {'header' :
+    {'unique_id': {'type': 'string', 'description': 'Typically UUID'},
+     'header' :
      {'type': 'object',
       'properties':
-      {'msg_id': {'type': 'string',
-                  'description': 'Typically UUID, must be unique per message'},
-       'session' : {'type': 'string',
-                    'description':
-                    'Typically UUID, should be unique per session'},
+      {'msg_id': {'$ref': '#/definitions/unique_id',
+                  'description':
+                  'Typically UUID, should be unique per message'},
+       'session' :  {'$ref': '#/definitions/unique_id',
+                     'description':
+                     'Typically UUID, should be unique per session'},
        'date': {'type': 'string',
                 'description':
                 'ISO 8601 timestamp for when the message is created'},
@@ -24,18 +26,22 @@ MESSAGE_SCHEMA = {
                     'default': '0.1',
                     'enum': ['0.1'],
                     'description': 'The message protocol version'}},
-      'required': ['msg_id', 'session', 'date', 'msg_type', 'version']}},
+      'required': ['msg_id', 'session', 'date', 'msg_type', 'version']},
+     'base_message':
+     {'description': 'ZeroMQ Plugin message format as json-schema (inspired '
+      'by IPython messaging format)',
+      'type': 'object',
+      'properties':
+      {'header': {'$ref': '#/definitions/header'},
+       # In a chain of messages, the header from the parent is copied so that
+       # clients can track where messages come from.
+       'parent_header': {'$ref': '#/definitions/header'},
+       'metadata': {'type': 'object',
+                    'description': 'Any metadata associated with the message.'},
 
-    'type': 'object',
-    'properties':
-    {'header': {r'$ref': '#/definitions/header'},
-     # In a chain of messages, the header from the parent is copied so that
-     # clients can track where messages come from.
-     'parent_header': {r'$ref': '#/definitions/header'},
-     'metadata': {'type': 'object',
-                  'description': 'Any metadata associated with the message.'},
+       'content': {'type': 'object',
+                   'description': 'The actual content of the message must be a '
+                   'dict, whose structure depends on the message type.'}},
+      'required': ['header', 'content']}},
 
-     'content': {'type': 'object',
-                 'description': 'The actual content of the message must be a '
-                 'dict, whose structure depends on the message type.'}},
-    'required': ['header', 'content']}
+    'allOf': [{'$ref': '#/definitions/base_message'}]}
