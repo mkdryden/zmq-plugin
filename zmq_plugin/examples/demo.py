cf5d35c52a372fae09a78604127379c68ec314e8
Christian Fobel <christian@fobel.net>
2015-12-18 14:01:52 -0500

    [WIP] Modify demo: remove send_command
    Modify demo to remove calls to `send_command` until command handling is
    updated to use JSON-schema messages (i.e., `execute_request`,
    `execute_reply`).

--- zmq_plugin/examples/demo.py
+++ zmq_plugin/examples/demo.py
@@ -4,6 +4,7 @@ import logging
 
 import zmq
 from zmq.eventloop import ioloop, zmqstream
+from tornado.ioloop import PeriodicCallback
 
 logger = logging.getLogger(__name__)
 
@@ -21,9 +22,16 @@ def run_hub(task):
     task.query_stream = zmqstream.ZMQStream(task.query_socket)
     task.query_stream.on_recv(task.on_query_recv)
 
+    def dump_registry():
+        print '\n' + (72 * '*') + '\n'
+        print task.registry
+        print '\n' + (72 * '*') + '\n'
+
     try:
         ioloop.install()
         logger.info('Starting hub ioloop')
+        PeriodicCallback(dump_registry, 100,
+                         io_loop=ioloop.IOLoop.instance()).start()
         ioloop.IOLoop.instance().start()
     except RuntimeError:
         logger.warning('IOLoop already running.')
@@ -73,27 +81,27 @@ if __name__ == '__main__':
     plugin_b = Plugin('plugin_b', 'tcp://localhost:12345')
     plugin_b.reset()
 
-    print '\n' + (72 * '=') + '\n'
+    #print '\n' + (72 * '=') + '\n'
 
-    for i in xrange(3):
-        # Send "ping" from `'plugin_b'` `'plugin_a'`
-        logger.info('''Send "ping" from `'plugin_b'` `'plugin_a'`''')
-        plugin_b.send_command('plugin_a', 'ping')
+    #for i in xrange(3):
+        ## Send "ping" from `'plugin_b'` `'plugin_a'`
+        #logger.info('''Send "ping" from `'plugin_b'` `'plugin_a'`''')
+        #plugin_b.send_command('plugin_a', 'ping')
 
-        logger.info('''Wait for command response to be received by `'plugin_b'`''')
-        frames = plugin_b.command_recv()
-        plugin_b.on_command_recv(frames)
+        #logger.info('''Wait for command response to be received by `'plugin_b'`''')
+        #frames = plugin_b.command_recv()
+        #plugin_b.on_command_recv(frames)
 
-        print '\n' + (72 * '-') + '\n'
+        #print '\n' + (72 * '-') + '\n'
 
-    print '\n# Plugin B subscribed message dump #\n'
+    #print '\n# Plugin B subscribed message dump #\n'
 
-    while True:
-        try:
-            logger.info(pprint.pformat(plugin_b.subscribe_socket
-                                       .recv_pyobj(zmq.NOBLOCK)))
-        except zmq.Again:
-            break
+    #while True:
+        #try:
+            #logger.info(pprint.pformat(plugin_b.subscribe_socket
+                                       #.recv_pyobj(zmq.NOBLOCK)))
+        #except zmq.Again:
+            #break
 
     plugin_process.terminate()
     hub_process.terminate()
