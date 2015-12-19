79540d923f4568ff179b9043814774840cbe98d3
Christian Fobel <christian@fobel.net>
2015-12-19 10:22:38 -0500

    [NB] Replace command handling w/ message handling
    Replace deprecated command handling logic with new
    `execute_request`/`execute_reply` message handling.

--- zmq_plugin/plugin.py
+++ zmq_plugin/plugin.py
@@ -1,14 +1,15 @@
 # coding: utf-8
-from pprint import pformat
-import re
-import logging
-import itertools
 from collections import OrderedDict
+from pprint import pformat
+import inspect
+import itertools
 import json
-import cPickle as pickle
-import yaml
+import logging
+import re
 
+import jsonschema
 import zmq
+
 from .schema import (validate, get_connect_request, get_execute_request,
                      get_execute_reply, decode_content_data)
 
@@ -29,19 +30,19 @@ class Plugin(object):
         self.query_socket = None
         self.command_socket = None
         self.subscribe_socket = None
+        self.execute_reply_id = itertools.count(1)
 
         self.callbacks = OrderedDict()
 
     @property
     def logger(self):
-        import inspect
 
         return logging.getLogger('.'.join((__name__, str(type(self).__name__),
                                            inspect.stack()[1][3]))
                                  + '->"%s"' % self.name)
 
     def reset(self):
-        self.command_msg_id = itertools.count()
+        self.execute_reply_id = itertools.count(1)
 
         self.reset_query_socket()
         connect_request = get_connect_request(self.name, 'hub')
@@ -106,26 +107,64 @@ class Plugin(object):
             self.reset_query_socket()
             raise
 
-    def send_command(self, target_name, data):
-        self.command_socket.send_multipart(['hub', target_name, '',
-                                            str(self.command_msg_id.next()), data])
+    def execute(self, target_name, command, callback=None, **kwargs):
+        request = get_execute_request(self.name, target_name, command,
+                                      data=kwargs)
+        self.command_socket.send_multipart(['hub', '', json.dumps(request)])
 
     def on_command_recv(self, frames):
-        self.logger.debug('in %s', frames)
         try:
-            a, b, null, msg_id = frames[:4]
-            msg = frames[4:]
-            command, args = msg[0], msg[1:]
-            if command == 'ping':
-                out_msg = [a, b, null, msg_id, 'pong']
-                self.logger.debug('out %s', out_msg)
-                self.command_socket.send_multipart(out_msg)
-            elif command == 'pong':
-                pass
+            message_str = frames[-1]
+            message = json.loads(message_str)
+            validate(message)
+        except jsonschema.ValidationError:
+            self.logger.error('unexpected message', exc_info=True)
+
+        self.logger.debug('in %s', frames)
+        message_type = message['header']['msg_type']
+        if message_type == 'execute_request':
+            self._process__execute_request(message)
+        elif message_type == 'execute_reply':
+            self._process__execute_reply(message)
+        else:
+            self.logger.error('Unrecognized message type: %s', message_type)
+
+    def _process__execute_reply(self, reply):
+        try:
+            session_id = reply['header']['session_id']
+            if session_id in self.callbacks:
+                self.logger.debug('Calling callback for session: %s',
+                                  session_id)
+                # A callback was registered for the corresponding request.
+                # Call callback with reply.
+                func = self.callbacks[session_id]
+                func(reply)
             else:
-                raise ValueError('Unknown command, "%s"' % command)
+                self.logger.warning('No callback registered for session: %s',
+                                    session_id)
         except:
-            self.logger.error('Unrecognized command structure.', exc_info=True)
+            self.logger.error('Processing error.', exc_info=True)
+
+    def _process__execute_request(self, request):
+        try:
+            func = getattr(self, 'on_execute__' +
+                           request['content']['command'], None)
+            if func is None:
+                error = NameError('Unrecognized command: %s' %
+                                  request['content']['command'])
+                reply = get_execute_reply(request,
+                                          self.execute_reply_id.next(),
+                                          error=error)
+            else:
+                reply = func(request)
+            validate(reply)
+            reply_str = json.dumps(reply)
+        except (Exception, ), exception:
+            reply = get_execute_reply(request, self.execute_reply_id.next(),
+                                      error=exception)
+            reply_str = json.dumps(reply)
+
+        self.command_socket.send_multipart(['hub', '', reply_str])
 
     def command_recv(self, *args, **kwargs):
         frames = self.command_socket.recv_multipart(*args, **kwargs)
